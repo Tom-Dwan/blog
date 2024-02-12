ac49335759a9ad0f3027798338c32e3c12392132
Tom-Dwan <53911219+Tom-Dwan@users.noreply.github.com>
2024-02-12 22:06:34 +0800

    Update 在 Win7 中安装 Node.js

--- 在 Win7 中安装 Node.js
+++ 在 Win7 中安装 Node.js
@@ -1,2 +1,3 @@
-《<a href=“https://www.cnblogs.com/wq2333/p/16501271.html">windows7安装nodejs以及更高版本</a>》
+根据《<a href=“https://www.cnblogs.com/wq2333/p/16501271.html">windows7安装nodejs以及更高版本</a>》，Win7 中只能安装到 v13 的最高版本。在 《<a 
+ href="https://nodejs.org/dist/">Index of /dist/</a>》下的《<a href="https://nodejs.org/dist/latest-v13.x/">Index of /dist/latest-v13.x/</a>》
 npm WARN read-shrinkwrap This version of npm is compatible with lockfileVersion@1, but package-lock.json was generated for lockfileVersion@3. I'll try to do my best with it!
